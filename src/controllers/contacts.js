a07ff566e88e800e7246efb00cd72b70863ff6a9
ssherliann <popova.ann05@gmail.com>
2024-06-06 12:11:20 +0300

    start

---
+++ src/controllers/contacts.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+import createHttpError from 'http-errors';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import { createContact, updateContact, deleteContact } from '../services/contacts.js';
+
+export const getContactsController = async(req, res, next) => {
+    try {
+        const contacts = await getAllContacts();
+
+        res.status(200).json({
+            status: 200,
+            message: 'Successfully found contacts!',
+            data: contacts,
+        });
+    } catch(error) {
+        next(error);
+    }
+
+};
+
+export const getContactByIdController = async(req, res, next) => {
+    const contactId = req.params.contactId;
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+        throw new Error('Invalid contact ID');
+    }
+
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+        next(createHttpError(404, 'Contact not found'));
+        return;
+    }
+
+    res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+    });
+};
+
+export const createContactController = async (req, res) => {
+    const contact = await createContact(req.body);
+
+    res.status(201).json({
+        status: 201,
+        message: `Successfully created a contact!`,
+        data: contact,
+    });
+};
+
+export const patchContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+    const result = await updateContact(contactId, req.body);
+
+    if (!result) {
+        next(createHttpError(404, 'Contact not found'));
+        return;
+    }
+    
+    res.json({
+        status: 200,
+        message: `Successfully patched a contact!`,
+        data: result.student,
+    });
+};
+
+export const deleteContactController = async (req, res, next) => {
+    const { contactId } = req.params;
+
+    const contact = await deleteContact(contactId);
+
+    if (!contact) {
+        next(createHttpError(404, 'Contact not found'));
+        return;
+    }
+    
+    res.status(204).send();
+};
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from 'express'; 
 import pino from 'pino-http';
 import cors from 'cors';
-import mongoose from 'mongoose';
-import { getAllContacts, getContactById } from './services/contacts.js';
 import { env } from './utils/env.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -21,63 +22,11 @@ export const setupServer = () => {
         }),
     );
 
-    app.get('/contacts', async (req, res) => {
-        try {
-            const contacts = await getAllContacts();
-    
-            res.status(200).json({
-                status: 200,
-                message: 'Successfully found contacts!',
-                data: contacts,
-            });
-            } catch (error) {
-                console.log('Something went wrong', error);
-            }
-    });
-    
-    app.get('/contacts/:contactId', async (req, res) => {
-        try {
-            const contactId = req.params.contactId;
-            if (!mongoose.Types.ObjectId.isValid(contactId)) {
-                throw new Error('Invalid contact ID');
-        }
-    
-        const contact = await getContactById(contactId);
-    
-        if (!contact) {
-            res.status(404).json({
-                status: 404,
-                message: 'Contact not found',
-            });
-            return;
-        }
-    
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        });
-        } catch (error) {
-            console.log('Contact not found!', error);
-            res.status(404).json({
-                status: 404,
-                message: 'Contact not found',
-            });
-        }
-    });
-    
-    app.use('*', (req, res) => {
-        res.status(404).json({
-            message: 'Not found',
-        });
-    });
-    
-    app.use((err, req, res) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-            error: err.message,
-        });
-    });
+    app.use(contactsRouter);
+
+    app.use('*', notFoundHandler);
+
+    app.use(errorHandler);
     
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
